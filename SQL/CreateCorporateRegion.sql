f6557a38243a6f682a108b5e81c8628f4f391b2a
K Truong <truong.k77@gmail.com>
2022-06-27 11:13:41 -0700

    Merge Customization Projects

---
+++ SQL/CreateCorporateRegion.sql
@@ -0,0 +1,10 @@
+IF NOT EXISTS(
+  SELECT * FROM sysobjects WHERE name='CorporateRegion' and xtype='U'
+)
+
+CREATE TABLE CorporateRegion
+(
+  CorporateRegionID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  CorporateRegionCD nvarchar(30) UNIQUE,
+  Descr nvarchar(60),
+)
---
+++ SQL/DropCorporateRegionTable.sql
@@ -0,0 +1,3 @@
+-- Uncomment the next line to run this script
+
+DROP TABLE IF EXISTS CorporateRegion;
